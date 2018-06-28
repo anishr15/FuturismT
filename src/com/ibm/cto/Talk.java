f7fdbc4947f43dc7102db27af3868ce25d035e93
Futurism T <chatbots.futurism@gmail.com>
2018-06-28 11:49:27 +0000

    Update Talk.java

--- src/com/ibm/cto/Talk.java
+++ src/com/ibm/cto/Talk.java
@@ -57,7 +57,7 @@ public class Talk extends HttpServlet {
 		response.setContentType("text/plain");
 		response.setCharacterEncoding("utf-8");
 		
-	    String var1 = contextMap.get("part_number").ToString();
+	    String var1 = contextMap.get("part_number").toString();
 	    System.out.println(var1);
 
 		ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_09_20);

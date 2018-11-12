20de8f7b89b932bd0e09e873d4dc19d981344df2
Futurism T <chatbots.futurism@gmail.com>
2018-11-12 10:45:54 +0000

    Update Talk.java

--- src/com/ibm/cto/Talk.java
+++ src/com/ibm/cto/Talk.java
@@ -64,9 +64,8 @@ public class Talk extends HttpServlet {
 
 		response.getWriter().append(r.toString());
 		/*contextMap = r.getContext();
-		
 		System.out.println(r.getContext().get("$name"));*/
-		if (r.getOutput().get("actions") ){
+		if (r.getOutput().get("actions")=="DBinsert"){
 		    System.out.println("inserted data");
 		}
 	

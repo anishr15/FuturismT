3b4fdac9ffea8e2380a634facc7e97a584e49fe0
Futurism T <chatbots.futurism@gmail.com>
2018-11-20 16:43:31 +0000

    JDBC changes to add data in database Talk.java

--- src/com/ibm/cto/Talk.java
+++ src/com/ibm/cto/Talk.java
@@ -14,6 +14,14 @@ import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 
 import com.alibaba.fastjson.JSONObject;
 
+// jdbc code packages
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 /**
  * Servlet implementation class Talk
  */
@@ -36,7 +44,10 @@ public class Talk extends HttpServlet {
 		String requestMessage = request.getParameter("message");
 		String contextString = request.getParameter("context");
 		JSONObject contextObject = new JSONObject();
-		
+
+// code changes		
+  Connection conn = null;
+  Statement stmt = null;
 
 		if(contextString != null) {
 			contextObject = JSONObject.parseObject(contextString);
@@ -84,6 +95,77 @@ public class Talk extends HttpServlet {
 		    System.out.println("inserted data");
 		}*/
 	
+		
+		
+		// jdbc code changes begin
+		
+		 Connection conn = null;
+  Statement stmt = null;
+
+        try {
+ 
+            String dbURL = "jdbc:sqlserver://169.62.148.19:1433;databaseName=ASAPChatBot_Staging;integratedSecurity=true"; //"jdbc:sqlserver://169.62.148.19\\ASAPChatBot_Staging";
+            String user = "ChatBot";
+            String pass = "$C#a!B0t1";
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            if (conn != null) {
+				// stmt = conn.createStatement();
+
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+
+
+				String insertTableSQL = "INSERT INTO RFQ_AsapChatbot"
+				+ "(RFQ_CustCompany, RFQ_CustEmail, RFQ_CustName, RFQ_CustPhone, RFQ_CustCompType, RFQ_MfgPartNo, RFQ_Quantity, RFQ_PartsBy, RFQ_Comments) VALUES"
+				+ "(?,?,?,?,?,?,?,?,?)";
+				PreparedStatement preparedStatement = conn.prepareStatement(insertTableSQL);
+				preparedStatement.setString(1, company);
+				preparedStatement.setString(2, company);
+				preparedStatement.setString(3, name);
+				preparedStatement.setString(4, phone);
+				preparedStatement.setString(5, companytype);
+				preparedStatement.setString(6, partnumber);
+				preparedStatement.setString(7, qty);
+				preparedStatement.setString(8, timeframe);
+				preparedStatement.setString(9, "Comments");
+
+				// execute insert SQL stetement
+				preparedStatement.executeUpdate();
+
+
+            }
+ 
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+		
+		// jbdc code changes end
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
